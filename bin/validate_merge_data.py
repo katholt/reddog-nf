79704d4883c898c5bae737ad1f49bc8e5a24e388
scwatts <s.watts2@student.unimelb.edu.au>
2020-07-13 11:01:38 +1000

    Clean up merge validation script

--- bin/validate_merge_data.py
+++ bin/validate_merge_data.py
@@ -28,24 +28,22 @@ def get_arguments():
 
 
 def main():
-    # Set rc, get commandline arguments
-    return_code = 0
+    # Get commandline arguments
     args = get_arguments()
+    status = {'return_code': 0, 'messages': list()}
 
     # Validate configuration
-    return_code_func = compare_run_configs(args.src_dir, args.dst_dir)
-    return_code |= return_code_func
+    compare_run_configs(args.src_dir, args.dst_dir, status)
 
     # Get reference data from run config and compare
-    return_code_func, merge_ref_name, merge_rep_names = compare_references(args.src_dir, args.dst_dir)
-    return_code |= return_code_func
+    merge_ref_name, merge_rep_names = compare_references(args.src_dir, args.dst_dir, status)
 
     # Using BAMs as ground truth - this could be changed to something else
     # Require at least one BAM
     isolate_names_bam = {fp.stem for fp in args.src_dir.glob('bams/*.bam')}
     if len(isolate_names_bam) < 1:
-        print(f'error: Found no BAM files in {args.src_dir}/bams/', file=sys.stderr)
-        return_code = 1
+        status['messages'].append(f'error: found no BAM files in {args.src_dir}/bams/')
+        status['return_code'] = 1
 
     # Compare isolate names between BAMs and VCFs
     isolate_names_vcf = set()
@@ -53,8 +51,8 @@ def main():
         isolate_names_vcf.add(vcf_fp.stem.replace('_q30', ''))
 
     if isolate_names_bam != isolate_names_vcf:
-        print('error: isolate names for BAMs and VCFs differ', file=sys.stderr)
-        return_code = 1
+        status['messages'].append('error: isolate names for BAMs and VCFs differ')
+        status['return_code'] = 1
 
     # Compare to fastqc if we have any
     isolate_names_fqc = set()
@@ -64,56 +62,54 @@ def main():
         assert re_result
         isolate_names_fqc.add(re_result.group(1))
     if isolate_names_fqc and isolate_names_fqc != isolate_names_bam:
-        print('error: isolate names in FastQC reports and BAMs differ', file=sys.stderr)
-        return_code = 1
+        status['messages'].append('error: isolate names in FastQC reports and BAMs differ')
+        status['return_code'] = 1
 
     # Check isolate names from gene coverage and depth
     gene_depth_fp = args.src_dir / f'{merge_ref_name}_gene_depth.tsv'
     gene_coverage_fp = args.src_dir / f'{merge_ref_name}_gene_coverage.tsv'
     if not gene_depth_fp.exists():
-        print(f'error: gene depth file {gene_depth_fp} does not exist', file=sys.stderr)
+        status['messages'].append(f'error: gene depth file {gene_depth_fp} does not exist')
+        status['return_code'] = 1
         isolate_names_depth = set()
-        return_code = 1
     else:
         with gene_depth_fp.open('r') as fh:
             isolate_names_depth = set(fh.readline().rstrip().split('\t')[2:])
     if not gene_coverage_fp.exists():
-        print(f'error: gene coverage file {gene_coverage_fp} does not exist', file=sys.stderr)
+        status['messages'].append(f'error: gene coverage file {gene_coverage_fp} does not exist')
+        status['return_code'] = 1
         isolate_names_coverage = set()
-        return_code = 1
     else:
         with gene_coverage_fp.open('r') as fh:
             isolate_names_coverage = set(fh.readline().rstrip().split('\t')[2:])
     if isolate_names_depth and isolate_names_depth != isolate_names_bam:
-        print('error: isolate names in gene depth and BAMs differ', file=sys.stderr)
-        return_code = 1
+        status['messages'].append('error: isolate names in gene depth and BAMs differ')
+        status['return_code'] = 1
     if isolate_names_coverage and isolate_names_coverage != isolate_names_bam:
-        print('error: isolate names in coverage depth and BAMs differ', file=sys.stderr)
-        return_code = 1
+        status['messages'].append('error: isolate names in coverage depth and BAMs differ')
+        status['return_code'] = 1
 
     # Check isolate names from mapping stats
-    return_code_func, isolate_names_stats = get_isolate_names_from_mapping_stats(args.src_dir, merge_rep_names, merge_ref_name)
-    return_code |= return_code_func
+    isolate_names_stats = get_isolate_names_from_mapping_stats(args.src_dir, merge_rep_names, merge_ref_name, status)
     for replicon_name, isolate_names in isolate_names_stats['all'].items():
         if isolate_names != isolate_names_bam:
-            print(f'error: isolate names in mapping stats ({replicon_name}) and BAMs differ', file=sys.stderr)
-            return_code = 1
+            status['messages'].append(f'error: isolate names in mapping stats ({replicon_name}) and BAMs differ')
+            status['return_code'] = 1
 
     # Get isolates from allele matrices and compare
-    return_code_func, isolate_names_allele = get_isolate_names_from_allele_matrices(args.src_dir, merge_rep_names, merge_ref_name)
-    return_code |= return_code_func
+    isolate_names_allele = get_isolate_names_from_allele_matrices(args.src_dir, merge_rep_names, merge_ref_name, status)
     allele_names = set(isolate_names_allele)
     stat_names = set(isolate_names_stats['pass'])
     missing_stats = allele_names.difference(stat_names)
     missing_allele = stat_names.difference(allele_names)
     if missing_stats:
         missing_stats_str = ','.join(missing_stats)
-        print(f'error: missing mapping stats files for {missing_stats_str}', file=sys.stderr)
-        return_code = 1
+        status['messages'].append(f'error: missing mapping stats files for {missing_stats_str}')
+        status['return_code'] = 1
     if missing_allele:
         missing_allele_str = ','.join(missing_allele)
-        print(f'error: missing allele matrix files for {missing_allele_str}', file=sys.stderr)
-        return_code = 1
+        status['messages'].append(f'error: missing allele matrix files for {missing_allele_str}')
+        status['return_code'] = 1
     # Only compare shared replicons and only check that allele isolate names are a subset of
     # mapping stats isolate names - isolates that pass but have no SNPs will not appear in the
     # allele matrix
@@ -122,45 +118,43 @@ def main():
         missing_isolates = isolate_names_allele[name].difference(isolate_names_stats['pass'][name])
         if missing_isolates:
             missing_isolates_str = ','.join(missing_isolates)
-            print(f'error: missing isolates {missing_isolates_str} in mapping stats for {name}', file=sys.stderr)
-            return_code = 1
+            status['messages'].append(f'error: missing isolates {missing_isolates_str} in mapping stats for {name}')
+            status['return_code'] = 1
 
     # Check for namespace collisions
     isolate_names_new = get_isolate_names_from_reads(args.read_globs)
     isolate_names_collisions = isolate_names_new & isolate_names_bam
     if isolate_names_collisions:
         names_str = ','.join(isolate_names_collisions)
-        print(f'error: detected isolate name collision for {names_str}', file=sys.stderr)
-        return_code = 1
+        status['messages'].append(f'error: detected isolate name collision for {names_str}')
+        status['return_code'] = 1
 
-    # Exit with explicit return code
-    sys.exit(return_code)
+    # Print all messages and exit
+    print_messages_and_exit(status)
 
 
-def compare_run_configs(src_dir, dst_dir):
-    return_code = 0
-    merge_rc, merge_data = collect_run_config_data(src_dir)
-    new_rc, new_data = collect_run_config_data(dst_dir)
-    return_code |= merge_rc
-    return_code |= new_rc
+def compare_run_configs(src_dir, dst_dir, status):
+    merge_data = collect_run_config_data(src_dir, status)
+    new_data = collect_run_config_data(dst_dir, status)
     if not new_data or not merge_data:
-        return 1
+        status['return_code'] = 1
+        return
     shared_vars = set(merge_data) & set(new_data)
     for var in shared_vars:
         if merge_data[var] != new_data[var]:
-            msg = f'error: merge run and current run have different settings for {var}:'
-            msg += f' merge: {merge_data[var]}; new: {new_data[var]}'
-            print(msg, file=sys.stderr)
-            return_code = 1
-    return return_code
+            message = f'error: merge run and current run have different settings for {var}:'
+            message += f' merge: {merge_data[var]}; new: {new_data[var]}'
+            status['messages'].append(message)
+            status['return_code'] = 1
 
 
-def collect_run_config_data(src_dir):
-    return_code = 0
+def collect_run_config_data(src_dir, status):
+    val_config = 0
     run_config_fp = src_dir / 'run_info/run_config.tsv'
     if not run_config_fp.exists():
-        print(f'error: aborting comparison: run config could not be found at {run_config_fp}', file=sys.stderr)
-        sys.exit(1)
+        message = f'error: aborting comparison: run config could not be found at {run_config_fp}'
+        status['messages'].append(message)
+        status['return_code'] = 1
     entries_skip = {
             'reference_name',
             'reference_replicon_names',
@@ -187,27 +181,26 @@ def collect_run_config_data(src_dir):
                 assert var not in run_config
                 run_config[var] = val
             else:
-                print(f'error: unexpected item in run_config {var}', file=sys.stderr)
-                return_code = 1
+                status['messages'].append(f'error: unexpected item in run_config {var}')
+                status['return_code'] = 1
     entries_missing = set(run_config) ^ entries_retain
     if entries_missing:
         entries_missing_str = ','.join(entries_missing)
-        print(f'error: {run_config_fp} has missing entries: {entries_missing_str}', file=sys.stderr)
-        return_code = 1
-    return return_code, run_config
+        status['messages'].append(f'error: {run_config_fp} has missing entries: {entries_missing_str}')
+        status['return_code'] = 1
+    return run_config
 
 
-def compare_references(src_dir, dst_dir):
-    return_code = 0
-    merge_data = collect_reference_info(src_dir)
-    new_data = collect_reference_info(dst_dir)
+def compare_references(src_dir, dst_dir, status):
+    merge_data = collect_reference_info(src_dir, status)
+    new_data = collect_reference_info(dst_dir, status)
     if not new_data or not merge_data:
-        return 1
+        status['return_code'] = 1
     # Reference name
     if new_data['name'] != merge_data['name']:
-        msg = f'error: reference name mismatch - merge: {merge_data["name"]}; new: {new_data["name"]}'
-        print(msg, file=sys.stderr)
-        return_code = 1
+        message = f'error: reference name mismatch - merge: {merge_data["name"]}; new: {new_data["name"]}'
+        status['messages'].append(message)
+        status['return_code'] = 1
     # Replicon names
     new_names = set(new_data['names'])
     merge_names = set(merge_data['names'])
@@ -215,34 +208,34 @@ def compare_references(src_dir, dst_dir):
         new_missing = ','.join(merge_names.difference(new_names))
         merge_missing = ','.join(new_names.difference(merge_names))
         if new_missing:
-            print(f'error: missing contigs in new reference {new_missing}', file=sys.stderr)
+            status['messages'].append(f'error: missing contigs in new reference {new_missing}')
         if merge_missing:
-            print(f'error: missing contigs in merge reference {merge_missing}', file=sys.stderr)
-        return_code = 1
+            status['messages'].append(f'error: missing contigs in merge reference {merge_missing}')
+        status['return_code'] = 1
     # Contig sizes and hashes (only those that are shared)
     rep_names = new_names & merge_names
     for name in rep_names:
         new_size = new_data['sizes'][name]
         merge_size = merge_data['sizes'][name]
         if new_size != merge_size:
-            msg = f'error: replicon size differs for {name} - merge {merge_size}; new {new_size}'
-            print(msg, file=sys.stderr)
-            return_code = 1
+            status['messages'].append(f'error: replicon size differs for {name} - merge {merge_size}; new {new_size}')
+            status['return_code'] = 1
             continue
         new_hash = new_data['hashes'][name]
         merge_hash = merge_data['hashes'][name]
         if new_hash != merge_hash:
-            msg = f'error: replicon hash (i.e. seq) differs for {name} - merge {merge_hash}; new {new_hash}'
-            print(msg, file=sys.stderr)
-            return_code = 1
-    return return_code, merge_data['name'], merge_data['names']
+            message = f'error: replicon hash (i.e. seq) differs for {name} - merge {merge_hash}; new {new_hash}'
+            status['messages'].append(message)
+            status['return_code'] = 1
+    return merge_data['name'], merge_data['names']
 
 
-def collect_reference_info(data_dir):
+def collect_reference_info(data_dir, status):
     run_config_fp = data_dir / 'run_info/run_config.tsv'
     if not run_config_fp.exists():
-        print(f'error: run config could not be found at {run_config_fp}', file=sys.stderr)
-        return
+        status['messages'].append(f'error: fatal: run config could not be found at {run_config_fp}')
+        status['return_code'] = 1
+        print_messages_and_exit(status)
     # Using different top level vars for robustness
     name = str()
     names = set()
@@ -273,8 +266,8 @@ def collect_reference_info(data_dir):
     return {'name': name, 'names': names, 'sizes': sizes, 'hashes': hashes}
 
 
-def get_isolate_names_from_mapping_stats(src_dir, merge_rep_names, merge_ref_name):
-    return_code = fps_count = 0
+def get_isolate_names_from_mapping_stats(src_dir, merge_rep_names, merge_ref_name, status):
+    fps_count = 0
     isolate_names_all = dict()
     isolate_names_pass = dict()
     for name in merge_rep_names:
@@ -299,8 +292,8 @@ def get_isolate_names_from_mapping_stats(src_dir, merge_rep_names, merge_ref_nam
                 if record.pass_fail == 'p':
                     isolate_names_pass[name].add(record.isolate)
     if fps_count < 1:
-        print('error: did not find any mapping stats file in merge directory', file=sys.stderr)
-        return_code = 1
+        status['messages'].append('error: did not find any mapping stats file in merge directory')
+        status['return_code'] = 1
     else:
         rep_names = list(isolate_names_all.keys())
         for i in range(len(rep_names)):
@@ -310,13 +303,13 @@ def get_isolate_names_from_mapping_stats(src_dir, merge_rep_names, merge_ref_nam
                 isolates_i = isolate_names_all[name_i]
                 isolates_j = isolate_names_all[name_j]
                 if isolates_i != isolates_j:
-                    print(f'error: isolates in mapping stats for {name_i} and {name_j} differ', file=sys.stderr)
-                    return_code = 1
-    return return_code, {'all': isolate_names_all, 'pass': isolate_names_pass}
+                    status['messages'].append(f'error: isolates in mapping stats for {name_i} and {name_j} differ')
+                    status['return_code'] = 1
+    return {'all': isolate_names_all, 'pass': isolate_names_pass}
 
 
-def get_isolate_names_from_allele_matrices(src_dir, merge_rep_names, merge_ref_name):
-    return_code = fps_count = 0
+def get_isolate_names_from_allele_matrices(src_dir, merge_rep_names, merge_ref_name, status):
+    fps_count = 0
     isolate_names = dict()
     for name in merge_rep_names:
         fp = src_dir / f'{merge_ref_name}_{name}_alleles.tsv'
@@ -329,9 +322,9 @@ def get_isolate_names_from_allele_matrices(src_dir, merge_rep_names, merge_ref_n
             isolates = set(fh.readline().rstrip().split('\t')[2:])
             isolate_names[name] = isolates
     if fps_count < 1:
-        print('error: did not find any allele matrices file in merge directory', file=sys.stderr)
-        return_code = 1
-    return return_code, isolate_names
+        status['messages'].append('error: did not find any allele matrices file in merge directory')
+        status['return_code'] = 1
+    return isolate_names
 
 
 def get_isolate_names_from_reads(read_globs):
@@ -351,6 +344,12 @@ def get_isolate_names_from_reads(read_globs):
             assert False
         isolate_names_new.add(re_result.group(1))
     return isolate_names_new
+
+
+def print_messages_and_exit(status):
+    for message in status['messages']:
+        print(message, file=sys.stderr)
+    sys.exit(status['return_code'])
 
 
 if __name__ == '__main__':
