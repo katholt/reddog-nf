e5d7ef95d3de4121d6302051667d241f26cdcca9
scwatts <s.watts2@student.unimelb.edu.au>
2020-07-14 12:09:46 +1000

    Framework remaining tests

---
+++ tests/test_miscellaneous.py
@@ -0,0 +1,95 @@
+import pathlib
+import unittest
+
+
+from . import tests_directory
+
+
+class AggregateSnpSites(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class CollectReferenceData(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class CreateCoverageDepthMatrix(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class CreateSnpAlignment(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class DetermineCodingConsequenes(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class FilterVariants(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class GenbankToFasta(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class GetPassingRepliconIsolates(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class MergeGeneStatMatrix(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
+
+
+class SplitMappedUnmappedReads(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full(self):
+        pass
